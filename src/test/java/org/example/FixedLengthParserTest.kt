4a873ffec5563246473f3d40146e739c30f7ee6b
Evelyn Soon <evelyn.soon@mail.utoronto.ca>
2024-08-07 18:39:34 -0400

    Issue X: Changed FixedLengthParserTest.

--- src/test/java/org/example/FixedLengthParserTest.kt
+++ src/test/java/org/example/FixedLengthParserTest.kt
@@ -1,66 +1,69 @@
-package org.example;
+package org.example
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class FixedLengthParserTest {
+internal class FixedLengthParserTest {
     @Test
-    void parseFile_valid1_data1() {
-        parseFile_valid1("src/test/resources/valid1_data1.txt");
+    fun parseFile_valid1_data1() {
+        parseFile_valid1("src/test/resources/valid1_data1.txt")
     }
 
     @Test
-    void parseFile_valid1_data2() {
-        parseFile_valid1("src/test/resources/valid1_data2.txt");
+    fun parseFile_valid1_data2() {
+        parseFile_valid1("src/test/resources/valid1_data2.txt")
     }
 
-    private void parseFile_valid1(String filePath) {
+    private fun parseFile_valid1(filePath: String) {
         try {
-            Generator generator = new Generator();
-            generator.generateRecordAndFLP("src/test/resources/valid1.schema");
+            val generator = Generator()
+            generator.generateRecordAndFLP("src/test/resources/valid1.schema")
 
-            FixedLengthParser testedParser = new FixedLengthParser();
+            val testedParser = FixedLengthParser()
+            val actualRecords = testedParser.parseFile(filePath)
 
-            List<Record> actualRecords = testedParser.parseFile(filePath);
+            val expectedRecords = mutableListOf<Record>()
+            val rec1 = Record("John Doe", "M", "25")
+            val rec2 = Record("Jane Smith", "F", "30")
+            expectedRecords.add(rec1)
+            expectedRecords.add(rec2)
 
-            List<Record> expectedRecords = new ArrayList<>();
-            Record rec1 = new Record("John Doe", "M", "25");
-            Record rec2 = new Record("Jane Smith", "F", "30");
-            expectedRecords.add(rec1);
-            expectedRecords.add(rec2);
-
-            assertEquals(expectedRecords.toString(), actualRecords.toString(), "parseFile_valid1_data1 should return the expected records.");
-            assertEquals(2, actualRecords.size(), "There should be 2 records.");
-        } catch (Exception e) {
+            assertEquals(
+                expectedRecords.toString(),
+                actualRecords.toString(),
+                "parseFile_valid1 should return the expected records."
+            )
+            assertEquals(2, actualRecords.size, "There should be 2 records.")
+        } catch (e: Exception) {
             // Fail the test if an exception is thrown
-            fail("Exception should not have been thrown: " + e.getMessage());
+            fail("Exception should not have been thrown: " + e.message)
         }
     }
 
 //    @Test
-//    void parseFile_valid2_data() {
+//    fun parseFile_valid2_data() {
 //        try {
-//            Generator generator = new Generator();
-//            generator.generateRecordAndFLP("src/test/resources/valid2.schema");
+//            val generator = Generator()
+//            generator.generateRecordAndFLP("src/test/resources/valid2.schema")
 //
-//            FixedLengthParser testedParser = new FixedLengthParser();
-//            List<Record> actualRecords = testedParser.parseFile("src/test/resources/valid2_data.txt");
+//            val testedParser = FixedLengthParser()
+//            val actualRecords = testedParser.parseFile("src/test/resources/valid2_data.txt")
 //
-//            List<Record> expectedRecords = new ArrayList<>();
-//            Record rec1 = new Record("2500", "12-12-2012");
-//            Record rec2 = new Record("100", "01-01-2024");
-//            expectedRecords.add(rec1);
-//            expectedRecords.add(rec2);
+//            val expectedRecords = mutableListOf<Record>()
+//            val rec1 = Record("2500", "12-12-2012")
+//            val rec2 = Record("100", "01-01-2024")
+//            expectedRecords.add(rec1)
+//            expectedRecords.add(rec2)
 //
-//            assertEquals(expectedRecords.toString(), actualRecords.toString(), "parseFile_valid2_data should return the expected records.");
-//            assertEquals(2, actualRecords.size(), "There should be 2 records.");
-//        } catch (Exception e) {
+//            assertEquals(
+//                expectedRecords.toString(),
+//                actualRecords.toString(),
+//                "parseFile_valid2 should return the expected records."
+//            )
+//            assertEquals(2, actualRecords.size, "There should be 2 records.")
+//        } catch (e: Exception) {
 //            // Fail the test if an exception is thrown
-//            fail("Exception should not have been thrown: " + e.getMessage());
+//            fail("Exception should not have been thrown: " + e.message)
 //        }
 //    }
 }
