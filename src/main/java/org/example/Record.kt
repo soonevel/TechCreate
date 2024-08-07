2d97b5ea640327f19aca3d8559634596476fa30e
Evelyn Soon <evelyn.soon@mail.utoronto.ca>
2024-08-07 16:58:00 -0400

    Issue X: Changed val to var in Record.

--- src/main/java/org/example/Record.kt
+++ src/main/java/org/example/Record.kt
@@ -1,6 +1,6 @@
 package org.example
 
-class Record(private val name: String, private val gender: String, private val age: String) {
+class Record(private var name: String, private var gender: String, private var age: String) {
     override fun toString(): String {
         return "Record {name='$name', gender='$gender', age='$age'}"
     }
