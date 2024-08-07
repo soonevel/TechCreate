64ca5d16082746ed098679189817d8390ac47f04
Evelyn Soon <evelyn.soon@mail.utoronto.ca>
2024-08-07 16:50:30 -0400

    Issue X: Changed Record.

--- src/main/java/org/example/Record.kt
+++ src/main/java/org/example/Record.kt
@@ -1,20 +1,7 @@
-package org.example;
+package org.example
 
-public class Record {
-    private String name;
-    private String gender;
-    private String age;
-
-    public Record (String name, String gender, String age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
+class Record(private val name: String, private val gender: String, private val age: String) {
+    override fun toString(): String {
+        return "Record {name='$name', gender='$gender', age='$age'}"
     }
-
-    @Override
-    public String toString() {
-        return "Record {name='" + name + "', gender='" + gender + "', age='" + age + "'}";
-    }
-
-    // Getters and Setters (if needed)
 }
