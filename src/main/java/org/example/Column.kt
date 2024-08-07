d2e81f82f5b5cb345276df7fd640fb73d74c7f4c
Evelyn Soon <evelyn.soon@mail.utoronto.ca>
2024-08-07 17:07:12 -0400

    Issue X: Changed Column.

--- src/main/java/org/example/Column.kt
+++ src/main/java/org/example/Column.kt
@@ -1,55 +1,10 @@
-package org.example;
+package org.example
 
-public class Column {
-    private String columnName;
-    private int startIndex;
-    private int endIndex;
-
-    /**
-     * Constructor
-     */
-    public Column(String columnName, int startIndex, int endIndex) {
-        this.columnName = columnName;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-    }
-
-    /**
-     * Getters and Setters
-     */
-    public String getColumnName() {
-        return columnName;
-    }
-
-    public void setColumnName(String columnName) {
-        this.columnName = columnName;
-    }
-
-    public int getStartIndex() {
-        return startIndex;
-    }
-
-    public void setStartIndex(int startIndex) {
-        this.startIndex = startIndex;
-    }
-
-    public int getEndIndex() {
-        return endIndex;
-    }
-
-    public void setEndIndex(int endIndex) {
-        this.endIndex = endIndex;
-    }
-
+class Column(val columnName: String, val startIndex: Int, val endIndex: Int) {
     /**
      * toString()
      */
-    @Override
-    public String toString() {
-        return "Column{" +
-                "columnName='" + columnName + '\'' +
-                ", startIndex=" + startIndex +
-                ", endIndex=" + endIndex +
-                '}';
+    override fun toString(): String {
+        return "Column(columnName='$columnName', startIndex=$startIndex, endIndex=$endIndex)"
     }
 }
--- src/main/java/org/example/Record.kt
+++ src/main/java/org/example/Record.kt
@@ -2,6 +2,6 @@ package org.example
 
 class Record(private var name: String, private var gender: String, private var age: String) {
     override fun toString(): String {
-        return "Record {name='$name', gender='$gender', age='$age'}"
+        return "Record(name='$name', gender='$gender', age='$age')"
     }
 }
